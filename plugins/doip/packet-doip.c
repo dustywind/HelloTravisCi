1ca1ed1080f833d3c6f1c6ff8ef94e520624c03e
Dustin Wind <dusty@posteo.de>
2016-04-25 20:36:23 +0200

    fixed an error in my impl

--- plugins/doip/packet-doip.c
+++ plugins/doip/packet-doip.c
@@ -3,6 +3,7 @@
 
 #include <epan/packet.h>
 
+
 #define DOIP_PORT 13400
 
 #define DOIP_FULLNAME "Diagnostic over IP"
@@ -14,8 +15,15 @@ static int proto_doip = -1;
 static void
 dissect_doip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
+    /* suppress warning for unused variables */
+    if(tvb) {
+        tvb = NULL;
+    }
+    if(tree) {
+        tree = NULL;
+    }
+
     col_set_str(pinfo->cinfo, COL_PROTOCOL, DOIP_SHORTNAME);
-    /* clear out stuff in the info column */
     col_clear(pinfo->cinfo, COL_INFO);
 }
 
@@ -36,7 +44,7 @@ proto_reg_handoff_doip(void)
     static dissector_handle_t doip_handle;
 
     doip_handle = create_dissector_handle(dissect_doip, proto_doip);
-    dissector_add_uint("tcp.dstport", DOIP_PORT, doip_handle);
+    dissector_add_uint("tcp.port", DOIP_PORT, doip_handle);
 }
 
 
